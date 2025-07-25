0477ddbbb5950e1fd81e24f78a88c181d5b36c46
Nayan Ghosh <138111956+nayanghosh-2002@users.noreply.github.com>
2025-07-26 03:56:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,9 @@ const authRoutes = require('./routes/authRoutes');
 const urlRoutes = require('./routes/urlRoutes');
 const redirectRoutes = require('./routes/redirectRoutes');
 
-// app.use('/api/auth', authRoutes);       
-app.use('/api/url', urlRoutes);         
-// app.use('/', redirectRoutes);           
+app.use('/api/auth', authRoutes);       
+// app.use('/api/url', urlRoutes);         
+app.use('/', redirectRoutes);           
 
 
 const PORT = process.env.PORT || 5000;

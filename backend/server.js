1bdf8b9e67681fc9ced9611bfce2ec8641710cea
Nayan Ghosh <138111956+nayanghosh-2002@users.noreply.github.com>
2025-07-26 03:55:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const authRoutes = require('./routes/authRoutes');
 const urlRoutes = require('./routes/urlRoutes');
 const redirectRoutes = require('./routes/redirectRoutes');
 
-app.use('/api/auth', authRoutes);       
+// app.use('/api/auth', authRoutes);       
 app.use('/api/url', urlRoutes);         
 // app.use('/', redirectRoutes);           
 

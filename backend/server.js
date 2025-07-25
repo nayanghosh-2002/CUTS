ec1057d4e4f0280471983039db12bcd407b3cf76
nayanghosh-2002 <138111956+nayanghosh-2002@users.noreply.github.com>
2025-07-26 03:27:06 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ connectDB();
 // Middleware
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://cuts-io.vercel.app/',
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
